ee083a8fdf4688a9a1de6fbff3463872083c2c01
Jules Dejaeghere <dej.j@live.be>
2023-12-24 11:32:01 +0100

    Set polling interval to 15 minutes

--- custom_components/irm_kmi/coordinator.py
+++ custom_components/irm_kmi/coordinator.py
@@ -27,7 +27,7 @@ class IrmKmiCoordinator(DataUpdateCoordinator):
             # Name of the data. For logging purposes.
             name="IRM KMI weather",
             # Polling interval. Will only be polled if there are subscribers.
-            update_interval=timedelta(seconds=30),
+            update_interval=timedelta(minutes=15),
         )
         self._api_client = IrmKmiApiClient(session=async_get_clientsession(hass))
         self._coord = coord
