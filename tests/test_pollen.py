43a207522aa14b3149fef6ce4252c883ec87473b
Jules Dejaeghere <dej.j@live.be>
2024-04-01 18:13:16 +0200

    Add tests

--- tests/test_pollen.py
+++ tests/test_pollen.py
@@ -1,7 +1,18 @@
 from custom_components.irm_kmi.pollen import PollenParser
 
+
 def test_svg_pollen_parsing():
-    # TODO make it an actual test
     with open("tests/fixtures/pollen.svg", "r") as file:
         svg_data = file.read()
-    PollenParser(svg_data).get_pollen_data()
+    data = PollenParser(svg_data).get_pollen_data()
+    assert data == {'birch': 'purple', 'oak': 'active', 'hazel': 'none', 'mugwort': 'none', 'alder': 'green',
+                    'grasses': 'none', 'ash': 'active'}
+
+
+def test_pollen_options():
+    assert PollenParser.get_option_values() == ['active', 'green', 'yellow', 'orange', 'red', 'purple', 'none']
+
+
+def test_pollen_default_values():
+    assert PollenParser.get_default_data() == {'birch': 'none', 'oak': 'none', 'hazel': 'none', 'mugwort': 'none',
+                                               'alder': 'none', 'grasses': 'none', 'ash': 'none'}
