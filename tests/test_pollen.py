8a93adb05384fc18fb5ec0a9c3331edc49898591
Jules Dejaeghere <dej.j@live.be>
2024-04-02 21:15:54 +0200

    Add test for pollen data fetching

--- tests/test_pollen.py
+++ tests/test_pollen.py
@@ -1,4 +1,11 @@
+from unittest.mock import AsyncMock
+
+from homeassistant.core import HomeAssistant
+from pytest_homeassistant_custom_component.common import MockConfigEntry
+
+from custom_components.irm_kmi import IrmKmiCoordinator
 from custom_components.irm_kmi.pollen import PollenParser
+from tests.conftest import get_api_data
 
 
 def test_svg_pollen_parsing():
@@ -16,3 +23,30 @@ def test_pollen_options():
 def test_pollen_default_values():
     assert PollenParser.get_default_data() == {'birch': 'none', 'oak': 'none', 'hazel': 'none', 'mugwort': 'none',
                                                'alder': 'none', 'grasses': 'none', 'ash': 'none'}
+
+
+async def test_pollen_data_from_api(
+        hass: HomeAssistant,
+        mock_svg_pollen: AsyncMock,
+        mock_config_entry: MockConfigEntry
+) -> None:
+    coordinator = IrmKmiCoordinator(hass, mock_config_entry)
+    api_data = get_api_data("be_forecast_warning.json")
+
+    result = await coordinator._async_pollen_data(api_data)
+    expected = {'mugwort': 'none', 'birch': 'purple', 'alder': 'green', 'ash': 'active', 'oak': 'active',
+                'grasses': 'none', 'hazel': 'none'}
+    assert result == expected
+
+
+async def test_pollen_error_leads_to_default_values(
+        hass: HomeAssistant,
+        mock_config_entry: MockConfigEntry,
+        mock_exception_irm_kmi_api_svg_pollen: AsyncMock
+) -> None:
+    coordinator = IrmKmiCoordinator(hass, mock_config_entry)
+    api_data = get_api_data("be_forecast_warning.json")
+
+    result = await coordinator._async_pollen_data(api_data)
+    expected = PollenParser.get_default_data()
+    assert result == expected
