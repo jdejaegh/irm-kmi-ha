cb32f77130094a1035ed7e202c28469b768c0be2
Jules Dejaeghere <dej.j@live.be>
2024-04-03 11:27:06 +0200

    Add test for SVG parsing

--- tests/test_pollen.py
+++ tests/test_pollen.py
@@ -15,6 +15,12 @@ def test_svg_pollen_parsing():
     assert data == {'birch': 'purple', 'oak': 'active', 'hazel': 'none', 'mugwort': 'none', 'alder': 'green',
                     'grasses': 'none', 'ash': 'active'}
 
+    with open("tests/fixtures/pollen_two.svg", "r") as file:
+        svg_data = file.read()
+    data = PollenParser(svg_data).get_pollen_data()
+    assert data == {'birch': 'purple', 'oak': 'active', 'hazel': 'none', 'mugwort': 'none', 'alder': 'active',
+                    'grasses': 'none', 'ash': 'active'}
+
 
 def test_pollen_options():
     assert PollenParser.get_option_values() == ['active', 'green', 'yellow', 'orange', 'red', 'purple', 'none']
